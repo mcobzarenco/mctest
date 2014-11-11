d4e1551d5d44b9c2431b0dd30b302b445383ab7d
Marius Cobzarenco <marius@reinfer.io>
2014-11-11 21:42:27 +0000

    parse to protobufs to reduce overhead

--- parse.py
+++ parse.py
@@ -13,6 +13,10 @@ from itertools import imap, ifilter
 
 import nltk
 import pandas as pd
+from google.protobuf import text_format
+
+from mctest_pb2 import AnswerAsWords, QuestionAsWords, StoryAsWords, \
+    AnswerAsEmbeddings, QuestionAsEmbeddings, StoryAsEmbeddings
 
 
 DEFAULT_OUTPUT_FORMAT = 'json'
@@ -41,7 +45,7 @@ def row_to_dict(row, tokenize=None):
     return {
         'id': row['id'],
         'description': row['description'],
-        'tokens': tokenize(row['story']),
+        'passage': tokenize(row['story']),
         'questions': [{
             'tokens': tokenize(question_text(row['q%d' % q_number])),
             'answers': [tokenize(row['a%d%d' % (q_number, a_number)])
@@ -51,47 +55,139 @@ def row_to_dict(row, tokenize=None):
     }
 
 
+
+def datapoint_to_proto_as_words(datapoint):
+    story = StoryAsWords()
+    story.id = datapoint['id']
+    story.description = datapoint['description']
+    story.passage.extend(datapoint['passage'])
+    for question_dict in datapoint['questions']:
+        question = story.questions.add()
+        if question_dict['type'] == 'one':
+            question.type = QuestionAsWords.ONE
+        elif question_dict['type'] == 'multiple':
+            question.type = QuestionAsWords.MULTIPLE
+        else:
+            print('Invalid question type: %s' % question_dict['type'],
+                  file=sys.stderr)
+            sys.exit(3)
+        question.tokens.extend(question_dict['tokens'])
+        for answer_list in question_dict['answers']:
+            answer = question.answers.add()
+            answer.tokens.extend(answer_list)
+    return story
+
+
+def datapoint_to_proto_as_embeddings(datapoint):
+    story = StoryAsEmbeddings()
+    story.id = datapoint['id']
+    story.description = datapoint['description']
+    for passage_vec in datapoint['passage']:
+        embed = story.passage.add()
+        embed.value.extend(list(passage_vec))
+    for question_dict in datapoint['questions']:
+        question = story.questions.add()
+        if question_dict['type'] == 'one':
+            question.type = QuestionAsWords.ONE
+        elif question_dict['type'] == 'multiple':
+            question.type = QuestionAsWords.MULTIPLE
+        else:
+            print('Invalid question type: %s' % question_dict['type'],
+                  file=sys.stderr)
+            sys.exit(3)
+        for token_vec in question_dict['tokens']:
+            question.tokens.add().value.extend(list(token_vec))
+        for answer_list in question_dict['answers']:
+            answer = question.answers.add()
+            for answer_vec in answer_list:
+                answer.tokens.add().value.extend(list(answer_vec))
+    return story
+
+
+def tokens_to_embeddings(model, tokens):
+    embeds = []
+    for token in tokens:
+        try:
+            token = token.lower()
+            embeds.append(model[token])
+        except KeyError as e:
+            print('WARNING: "%s" missing from vocabulary.' % token,
+                  file=sys.stderr)
+    return embeds
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Converts raw TSV files from the MCTest dataset')
     _arg = parser.add_argument
     _arg('-o', type=str, action='store', metavar='FORMAT',
          default=DEFAULT_OUTPUT_FORMAT,
-         help='Output format: json (default=%s)' %  DEFAULT_OUTPUT_FORMAT)
+         help='Output format: json, proto, prototext (default=%s)' %
+         DEFAULT_OUTPUT_FORMAT)
     _arg('--rm-stop', type=str, action='store', metavar='FILE',
          help='Remove stop words specified by file (one word per line).')
     _arg('--rm-punct', action='store_true',
          help='Remove punctuation when tokenizing.')
+    _arg('--model-file', type=str, action='store', metavar='FILE', default=None,
+         help='File with word2vec model. If provided, makes it output' \
+         'embeddings.')
     _arg('-i', type=str, action='store', metavar='FILE', default=None,
          help='Input file (TSV).')
     args = parser.parse_args()
 
-    token_filters = []
+    token_mappers = []
+
     if args.rm_stop:
         stopwords = open(args.rm_stop, 'r').read().split('\n')
         stopwords = set(map(lambda x: x.strip().rstrip(), stopwords))
-        token_filters.append(lambda x: x.lower() not in stopwords)
+        token_mappers.append(lambda x: x if x.lower() not in stopwords else None)
+
     if args.rm_punct:
-        token_filters.append(lambda x: x not in PUNCTS)
+        token_mappers.append(lambda x: x if x not in PUNCTS else None)
+
+    as_embeddings = args.model_file is not None
+    if args.model_file:
+        import word2vec
+        embedding_model = word2vec.load(args.model_file)
+        def to_embeddings(token):
+            try:
+                return embedding_model[token.lower()]
+            except KeyError as e:
+                print('WARNING: "%s" missing from vocabulary.' % token,
+                      file=sys.stderr)
+            return None
+        token_mappers.append(to_embeddings)
 
     def tokenize(text):
         if not isinstance(text, basestring):
             text = str(text)
         text = text.replace('\\newline', ' ')
-        return filter(lambda t: all(map(lambda f: f(t), token_filters)),
-                      nltk.word_tokenize(text))
+        mapped = nltk.word_tokenize(text)
+        for mapper in token_mappers:
+            mapped = filter(lambda x: x is not None, map(mapper, mapped))
+        return mapped
 
-    with (open(args.i, 'r') if args.i else sys.stdin) as fin:
-        df = pd.read_csv(fin, sep='\t', names=COLUMNS)
-        if args.o == 'json':
-            try:
-                for row in (df.ix[i] for i in df.index):
-                    datapoint = row_to_dict(row, tokenize)
-                    print(json.dumps(datapoint), file=sys.stdout)
-            except IOError as e:
-                if e.errno == errno.EPIPE:
-                    sys.exit(0)
-                raise e
-        else:
-            print('Unknown output format "%s"' % args.o, file=sys.stderr)
-            sys.exit(2)
+    data_in = open(args.i, 'r') if args.i else sys.stdin
+    df = pd.read_csv(data_in, sep='\t', names=COLUMNS)
+    for row in (df.ix[i] for i in df.index):
+        datapoint = row_to_dict(row, tokenize)
+        try:
+            serialized = None
+            if args.o == 'json':
+                serialized = json.dumps(datapoint)
+            elif args.o == 'proto' or args.o == 'prototext':
+                proto = datapoint_to_proto_as_embeddings(datapoint) \
+                        if as_embeddings else \
+                           datapoint_to_proto_as_words(datapoint)
+                serialized = proto.SerializeToString() if args.o == 'proto' \
+                             else text_format.MessageToString(proto)
+            else:
+                print('Unknown output format "%s"' % args.o,
+                      file=sys.stderr)
+                sys.exit(2)
+            assert serialized
+            print(serialized, file=sys.stdout)
+        except IOError as e:
+            if e.errno == errno.EPIPE:
+                sys.exit(0)
+            raise e
