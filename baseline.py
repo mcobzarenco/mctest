b9d5cc5581423f90eafe6956c9ad65cecd2cf911
Marius Cobzarenco <marius@reinfer.io>
2014-11-14 17:26:49 +0000

    read stories saved as protos

--- baseline.py
+++ baseline.py
@@ -4,6 +4,7 @@ from __future__ import print_function, division
 import argparse
 import json
 import os
+import struct
 import sys
 from base64 import urlsafe_b64decode as b64decode, \
     urlsafe_b64encode as b64encode
@@ -13,33 +14,29 @@ from itertools import imap
 
 import numpy as np
 
+from mctest_pb2 import StoryAsWords, QuestionAsWords
+
 
 ANSWER_LETTER = ['A', 'B', 'C', 'D']
 
 
-def compute_counts(passages):
+def compute_counts(stories):
     counts = defaultdict(lambda: 0.0)
-    for passage in passages:
-        for token in passage['tokens']:
+    for story in stories:
+        for token in story.passage:
             counts[token] += 1.0
     return counts
 
 
-def compute_inverse_counts(passages):
-    counts = compute_counts(passages)
+def compute_inverse_counts(stories):
+    counts = compute_counts(stories)
     icounts = {}
-    for (token, token_count) in counts.iteritems():
+    for token, token_count in counts.iteritems():
         icounts[token] = np.log(1.0 + 1.0 / token_count)
     return icounts
 
 
-def load_target_answers(stream):
-    answers = stream.readlines()
-    answers = map(lambda x: x.rstrip().split('\t'), answers)
-    return reduce(lambda x, y: x + y, answers)
-
-
-def answer_distance(passage, question, answer):
+def baseline_distance(passage, question, answer):
     if not isinstance(question, set):
         question = set(question)
     if not isinstance(answer, set):
@@ -57,7 +54,8 @@ def answer_distance(passage, question, answer):
             last_a = i
         if abs(last_q - last_a) < closest:
             # print(last_q, last_a)
-            closest = (np.abs(last_q - last_a) + 1) / len(passage)
+            closest = np.abs(last_q - last_a) / (len(passage) - 1)
+    assert closest > 0 and closest <= 1
     return closest
 
 
@@ -65,8 +63,8 @@ class SlidingWindow(object):
     def __init__(self):
         pass
 
-    def fit(self, passages, window_size=None):
-        self._icounts = compute_inverse_counts(passages)
+    def fit(self, stories, window_size=None):
+        self._icounts = compute_inverse_counts(stories)
         self._window_size = window_size
 
     def predict_target(self, tokens, target, verbose=True):
@@ -85,27 +83,46 @@ class SlidingWindow(object):
             except IndexError:
                 pass
             if overlap_score > max_overlap_score:
-                tokens_at_max = tokens[i:i + target_size]
+                tokens_at_max = tokens[i:i + window_size]
                 max_overlap_score = overlap_score
         if verbose:
             print('[score=%.2f for target=%s] passage: %s ' %
                   (max_overlap_score, target, tokens_at_max), file=sys.stderr)
         return max_overlap_score
 
-    def predict(self, story, verbose=True):
-        p_tokens = story['tokens']
-        answers = []
-        for question in story['questions']:
-            scores = []
-            q_tokens = question['tokens']
-            if verbose:
-                print('Question: %s' % q_tokens)
-            for answer in question['answers']:
-                dist = answer_distance(p_tokens, q_tokens, answer)
-                scores.append(self.predict_target(
-                    p_tokens, set(q_tokens + answer), verbose) - dist)
-            answers.append(scores)
-        return answers
+    def predict(self, passage, question, answers,
+                with_distance=True, verbose=True):
+        scores = []
+        if verbose:
+            print('Question: %s' % question)
+        for answer in answers:
+            dist = baseline_distance(passage, question, answer) \
+                   if with_distance else 0
+            scores.append(self.predict_target(
+                passage, set(question + answer), verbose) - dist)
+        return scores
+
+
+def load_target_answers(stream):
+    answers = stream.readlines()
+    answers = map(lambda x: x.rstrip().split('\t'), answers)
+    return reduce(lambda x, y: x + y, answers)
+
+
+def read_stories(stream):
+    stories = []
+    while True:
+        proto_size_bin = stream.read(4)
+        if len(proto_size_bin) != 4:
+            if len(proto_size_bin) == 0:
+                return stories
+            print('Invalid read: rubbish at the end of the file?',
+                  file=sys.stderr)
+            return stories
+        proto_size = struct.unpack_from('I', proto_size_bin)[0]
+        story = StoryAsWords()
+        story.ParseFromString(stream.read(proto_size))
+        stories.append(story)
 
 
 if __name__ == '__main__':
@@ -117,27 +134,52 @@ if __name__ == '__main__':
          help='File with stories and questions (JSON format).')
     _arg('--truth', type=str, action='store', metavar='FILE',
          help='File with correct answers to the questions.')
+    _arg('--window-size', type=int, action='store', metavar='SIZE',
+         default=None, help='Fixed window size for the sliding window ' \
+         'algorithm. By default it has the same length as the question.')
+    _arg('--distance', action='store_true',
+         help='Substract the baseline distance measure.')
     args = parser.parse_args()
 
-    stories = []
-    with open(args.train, 'r') as train_in:
-        stories.extend(map(json.loads, train_in.readlines()))
+    stories = read_stories(open(args.train, 'r'))
+    print('[model]\nwindow_size = %s\ndistance = %s\n' %
+          (args.window_size, args.distance))
 
-    for ws in xrange(5, 30):
-        sw = SlidingWindow()
-        sw.fit(stories, window_size=None)
-        predicted = []
-        for story in stories[:]:
-            for a in sw.predict(story, False):
-                predicted.append(ANSWER_LETTER[a.index(max(a))])
+    sw = SlidingWindow()
+    sw.fit(stories, window_size=args.window_size)
+    predicted, q_types = [], []
+    for story in stories:
+        passage = story.passage
+        for question in story.questions:
+            q_types.append(question.type)
+            answer_tokens = map(lambda x: list(x.tokens), question.answers)
+            scores = sw.predict(passage, list(question.tokens), answer_tokens,
+                                with_distance=args.distance, verbose=False)
+            predicted_letter = ANSWER_LETTER[scores.index(max(scores))]
+            # print('scores: %s (%s)' % (scores, predicted_letter))
+            predicted.append(predicted_letter)
 
-        if args.truth:
-            answers_in = open(args.truth, 'r')
-            answers = np.array(load_target_answers(answers_in))
-            predicted = np.array(predicted)
-            assert len(answers) == len(predicted)
-            print('[ws=%d] Accuracy: %.4f' %
-                  (ws, np.sum(answers == predicted) / float(len(predicted))))
-        break
-    # target = set( + q['questions'][0]['answers'][1])
-    # sliding_window_overlap(icounts, questions[0]['passage'], target)
+    if args.truth:
+        answers_in = open(args.truth, 'r')
+        answers = np.array(load_target_answers(answers_in))
+        predicted = np.array(predicted)
+        assert len(answers) == len(predicted)
+
+        single = np.array(q_types) == QuestionAsWords.ONE
+        n_single = float(np.sum(single))
+        n_multiple = float(np.sum(~single))
+        assert n_single + n_multiple == len(answers)
+
+        print('[results]')
+        print('All accuracy [%d]: %.4f' %
+              (n_single + n_multiple,
+               np.sum(answers == predicted) / float(len(predicted))))
+        print('Single accuracy [%d]: %.4f' %
+              (n_single,
+               np.sum(answers[single] == predicted[single]) / n_single))
+        print('Multiple accuracy [%d]: %.4f' %
+              (n_multiple,
+               np.sum(answers[~single] == predicted[~single]) / n_multiple))
+    else:
+        for p in predicted:
+            print(p, file=sys.stdout)
